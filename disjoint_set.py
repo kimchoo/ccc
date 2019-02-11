7252f785272c18e4b29fb8aa472c4fb8cd867c2c
kimchoo <34254826+kimchoo@users.noreply.github.com>
2019-02-11 13:08:02 -0500

    Create disjoint_set.py

---
+++ disjoint_set.py
@@ -0,0 +1,13 @@
+class solution:
+  def __init__(self,input):
+    self.input = [[4,4],[1,2],[1,3],[2,3],[3,4]]
+    self.output = []
+    self.spanning = False
+    
+  def kruskal(self):
+    edge = 0
+    while (self.spanning == False) and (len(self.input) != len(0)):
+      edge +=1
+      if 
+        self.input.pop(edge)
+        
